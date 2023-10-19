13b158525841d9f7bc498a89e21ca378d47398f8
anthony.aumond <anthony.aumond@mobiapps.fr>
2023-10-19 22:12:09 +0200

    changes

--- lib/ui/pages/home.page.dart
+++ lib/ui/pages/home.page.dart
@@ -54,14 +54,13 @@ class HomePage extends StatelessWidget {
                   childAspectRatio: 1.6,
                 ),
                 itemCount: pokemons.length,
-                itemBuilder: (context, index) {
-                  final pokemon = pokemons.elementAt(index);
+                itemBuilder: (BuildContext context, int index) {
+                  final Pokemon pokemon = pokemons.elementAt(index);
                   return PokemonCardWidget(
                     pokemon: pokemon,
                     onTap: () => Navigator.of(context).push(
                       MaterialPageRoute(
-                        builder: (context) =>
-                            PokemonDetailsPage(pokemon: pokemon),
+                        builder: (_) => PokemonDetailsPage(pokemon: pokemon),
                       ),
                     ),
                   );
@@ -74,8 +73,10 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  void _allTypesDialog(BuildContext context,
-      {required List<Pokemon> pokemons}) async {
+  void _allTypesDialog(
+    BuildContext context, {
+    required List<Pokemon> pokemons,
+  }) async {
     final Map<String, PokemonType> typesByName = {};
 
     pokemons.expand((Pokemon pokemon) => pokemon.types).forEach(
@@ -116,8 +117,10 @@ class HomePage extends StatelessWidget {
       );
   }
 
-  void _allGenerationsDialog(BuildContext context,
-      {required List<Pokemon> pokemons}) async {
+  void _allGenerationsDialog(
+    BuildContext context, {
+    required List<Pokemon> pokemons,
+  }) async {
     final int? generation = await showModalBottomSheet<int>(
       isScrollControlled: true,
       constraints: BoxConstraints(
