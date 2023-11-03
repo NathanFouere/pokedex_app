bce87f229c9c0501b6e685583b4f018d5927668d
anthony.aumond <anthony.aumond@mobiapps.fr>
2023-11-03 11:20:29 +0100

    changes

--- lib/ui/pages/home.page.dart
+++ lib/ui/pages/home.page.dart
@@ -13,6 +13,7 @@ import 'package:pokedex_app/ui/utils/int.extension.dart';
 import 'package:pokedex_app/ui/widgets/home_fab.widget.dart';
 import 'package:pokedex_app/ui/widgets/home_header.widget.dart';
 import 'package:pokedex_app/ui/widgets/pokemon_card.widget.dart';
+import 'package:shimmer/shimmer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -40,49 +41,14 @@ class _HomePageState extends State<HomePage> {
         child: BlocBuilder<PokemonsCubit, PokemonsState>(
           builder: (BuildContext context, PokemonsState state) {
             Widget content;
-            List<Pokemon> pokemons = <Pokemon>[];
+            List<Pokemon> pokemons = Pokemon.mocks();
 
             if (state is FailureState) {
               content = const Center(
                 child: Text('Une erreur est survenue !'),
               );
-            } else if (state is LoadingState) {
-              pokemons = Pokemon.mocks();
-              content = const Center(
-                child: CircularProgressIndicator(),
-              );
             } else if (state is SuccessState<PokemonsStateData>) {
               pokemons = state.data.pokemons;
-              content = GridView.builder(
-                padding: const EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 20,
-                    ) +
-                    EdgeInsets.only(
-                      bottom: MediaQuery.paddingOf(context).bottom,
-                    ),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 10,
-                  crossAxisSpacing: 10,
-                  childAspectRatio: 1.4,
-                ),
-                itemCount: pokemons.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final Pokemon pokemon = pokemons.elementAt(index);
-                  return PokemonCardWidget(
-                    pokemon: pokemon,
-                    onTap: () => Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (_) => BlocProvider<PokemonsCubit>.value(
-                          value: _pokemonsCubit,
-                          child: PokemonDetailsPage(pokemon: pokemon),
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              );
             } else {
               content = const SizedBox();
             }
@@ -112,7 +78,41 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                     Expanded(
-                      child: content,
+                      child: GridView.builder(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 20,
+                        ) +
+                            EdgeInsets.only(
+                              bottom: MediaQuery.paddingOf(context).bottom,
+                            ),
+                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 10,
+                          crossAxisSpacing: 10,
+                          childAspectRatio: 1.4,
+                        ),
+                        itemCount: pokemons.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final Pokemon pokemon = pokemons.elementAt(index);
+                          return Shimmer.fromColors(
+                            enabled: state is LoadingState,
+                            baseColor: Colors.grey,
+                            highlightColor: Colors.blueGrey,
+                            child: PokemonCardWidget(
+                              pokemon: pokemon,
+                              onTap: () => Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (_) => BlocProvider<PokemonsCubit>.value(
+                                    value: _pokemonsCubit,
+                                    child: PokemonDetailsPage(pokemon: pokemon),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
                     ),
                   ],
                 ),
