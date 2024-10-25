1d292933072771f6f232d999ff944e04658fdbf5
Nathan Fouéré <nathanfouere@tutanota.com>
2024-10-25 09:59:26 +0200

    add statefull example

--- lib/ui/pages/home.page.dart
+++ lib/ui/pages/home.page.dart
@@ -5,10 +5,39 @@ import 'package:pokedex_app/ui/pages/pokemon_details.page.dart';
 import 'package:pokedex_app/ui/widgets/home_header.widget.dart';
 import 'package:pokedex_app/ui/widgets/pokemon_card.widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({
     super.key,
   });
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isLoading = false;
+  List<Pokemon> pokemons = [];
+
+  @override
+  void initState() {
+    super.initState();
+    this.getPokemons();
+  }
+
+  Future<void> getPokemons() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    List<Pokemon> listOfPokemons = await PokemonService().getAll();
+
+    setState(() {
+      pokemons = listOfPokemons;
+      isLoading = false;
+    });
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -28,50 +57,37 @@ class HomePage extends StatelessWidget {
               ),
             ),
             Expanded(
-              child: FutureBuilder<List<Pokemon>>(
-                  future: futurePok,
-                  builder: (BuildContext context, AsyncSnapshot<List<Pokemon>> snapshot) {
-                    if (snapshot.hasData) {
-                      List<Pokemon> pokemons = snapshot.data ?? [];
-                      return GridView.builder(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 20,
-                        ) +
-                            EdgeInsets.only(
-                              bottom: MediaQuery.paddingOf(context).bottom,
-                            ),
-                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          mainAxisSpacing: 10,
-                          crossAxisSpacing: 10,
-                          childAspectRatio: 1.5,
-                        ),
-                        itemCount: pokemons.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          final Pokemon pokemon = pokemons.elementAt(index);
-                          return PokemonCardWidget(
-                            pokemon: pokemon,
-                            onTap: () => Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (_) => PokemonDetailsPage(pokemon: pokemon),
-                              ),
-                            ),
-                          );
-                        },
-                      );
-                    } else if (snapshot.hasError) {
-                      print(snapshot.stackTrace);
-                      return Center(
-                        child: Text(snapshot.error.toString()),
-                      );
-                    }  
-                    else {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                  },
+              child:
+                isLoading ?
+                  const Center(
+                    child: CircularProgressIndicator(),
+                  ) :
+              GridView.builder(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 20,
+                ) +
+                    EdgeInsets.only(
+                      bottom: MediaQuery.paddingOf(context).bottom,
+                    ),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                  childAspectRatio: 1.5,
+                ),
+                itemCount: pokemons.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final Pokemon pokemon = pokemons.elementAt(index);
+                  return PokemonCardWidget(
+                    pokemon: pokemon,
+                    onTap: () => Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) => PokemonDetailsPage(pokemon: pokemon),
+                      ),
+                    ),
+                  );
+                },
               ),
             ),
           ],
